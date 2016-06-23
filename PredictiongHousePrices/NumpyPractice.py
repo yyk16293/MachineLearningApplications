73b95869f9c4f6bad546b1cb6e2eec0f7b76bd38
yyk16293 <yyk16293@gmail.com>
2016-06-22 22:34:25 -0700

    initializing and using 2D arrays in numpy

--- PredictiongHousePrices/NumpyPractice.py
+++ PredictiongHousePrices/NumpyPractice.py
@@ -6,3 +6,11 @@ sample_np_array = np.array(my_list)
 
 print sample_np_array
 print type(sample_np_array)
+
+
+one2Darray = np.ones((4, 3)) # an 2D array with 4 "rows" and 3 "columns"
+print one2Darray
+print one2Darray.shape
+
+#printing third element in rows 0 and 1
+print one2Darray[0:2,2]
