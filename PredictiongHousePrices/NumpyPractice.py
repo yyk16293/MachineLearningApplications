4953b11dc07bfa3d2b7035fbe9a390bc1f73cb72
yyk16293 <yyk16293@gmail.com>
2016-06-23 23:31:01 -0700

    adding code for multiplying matrices

--- PredictiongHousePrices/NumpyPractice.py
+++ PredictiongHousePrices/NumpyPractice.py
@@ -7,10 +7,23 @@ sample_np_array = np.array(my_list)
 print sample_np_array
 print type(sample_np_array)
 
-
 one2Darray = np.ones((4, 3)) # an 2D array with 4 "rows" and 3 "columns"
 print one2Darray
 print one2Darray.shape
 
 #printing third element in rows 0 and 1
 print one2Darray[0:2,2]
+
+#dot product of arrays
+array1 = np.array([2., 3., 4., 5.])
+array2 = np.array([2., 3., 4., 5.])
+print np.dot(array1, array2)
+
+#product of arrays
+print array1*array2
+
+print np.sum(array1*array2)
+
+#Euclidean length of a vector
+print np.sqrt(np.dot(array1,array2))
+print np.sqrt(np.sum(array1*array2))
