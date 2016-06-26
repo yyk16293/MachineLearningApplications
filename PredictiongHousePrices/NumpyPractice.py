bea716643ff15c8f5666f3c6aab6aa8f799b0477
yyk16293 <yyk16293@gmail.com>
2016-06-25 21:42:08 -0700

    adding code for multiplication of 2-D Matrices

--- PredictiongHousePrices/NumpyPractice.py
+++ PredictiongHousePrices/NumpyPractice.py
@@ -27,3 +27,13 @@ print np.sum(array1*array2)
 #Euclidean length of a vector
 print np.sqrt(np.dot(array1,array2))
 print np.sqrt(np.sum(array1*array2))
+
+#Multiplication of 2-D matrices
+matrix_1 = np.array([[3, 6, 9],[14, 28, 42]])
+print matrix_1
+print matrix_1.shape #2 rows, 3 columns
+
+matrix_2 = np.array([[1, 2], [3, 4], [5, 6]])
+print matrix_2.shape #3 rows, 2 columns
+
+print np.dot(matrix_1,matrix_2) #2 rows, 2 columns
